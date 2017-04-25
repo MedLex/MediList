fa865755a65be431f81681b347379125eb6237b6
WimEngberts <wim.engberts@centric.eu>
2017-04-25 13:53:59 +0200

    Add files via upload

---
+++ js/MediApp.js
@@ -0,0 +1,85 @@
+function onClickMenu()
+{
+	alert ('u hebt op het menu-knopke gedruk!!!!');
+}
+
+function buildOverzicht ()
+{
+	
+	if (!isDeviceReady ())
+	{
+		alert ('Cordova is nog niet geladen....');
+		return ;
+	}
+	var db = sqlitePlugin.openDatabase (
+		{
+			name: "medi.db",
+			location: 'default'
+		});
+	initTables (db);
+	db.executeSql ('select * from overzichten;', [], function (res)
+	{
+		for (var i=0; i < res.count; i++)
+		{
+			alert (res[i][0]);
+		}
+	});
+	
+	db.close ();
+}
+
+function initTables (db)
+{
+	db.transaction (function (tx)
+	{
+		tx.executeSql ('CREATE TABLE IF NOT EXISTS overzichten(id integer primary key,'
+														    + 'apotheek text,'
+		                                                    + 'datum text,'
+														    + 'patient text'
+														    + 'geboren text)');
+		tx.executeSql ('CREATE TABLE IF NOT EXISTS medicatie  (overzicht integer,'
+														    + 'regel integer,'
+		                                                    + 'datum text,'
+														    + 'voorschrijver text'
+															+ 'medicijn text,'
+															+ 'dosering text,'
+															+ 'start text,'
+															+ 'end text,'
+															+ 'duur integer,'
+															+ 'toediening text,'
+															+ 'toelichting text,'
+															+ 'herhaling bool,'
+														    + 'code text)');
+	}, function (error)
+	{
+		alert ('er is een fout opgetreden\r\n' + error.message);
+	});
+}
+
+function onShowMed (vIndex)
+{
+	if (vIndex == 1)
+		ShowPrescription ('pantozaprol tablet msr 20mg',
+	                        '<tr><td>Startdatum</td><td>:</td><td>18-09-2014</td></tr>'
+	                      + '<tr><td>Stopdatum</td><td>:</td><td></td></tr>'
+	                      + '<tr><td>Dosering</td><td>:</td><td>1 x per dag 40 milligram</td></tr>'
+	                      + '<tr><td>Toelichting</td><td>:</td><td></td></tr>'
+	                      + '<tr><td>Toediening</td><td>:</td><td>Oraal</td></tr>'
+	                      + '<tr><td>Voorschrijver</td><td>:</td><td>Lorsheyd, A<br />CAR 03053035</td></tr>');
+	else if (vIndex == 2)
+		ShowPrescription ('carbasalaatcalcium bruistablet 100mg',
+	                        '<tr><td>Startdatum</td><td>:</td><td>07-01-2013</td></tr>'
+	                      + '<tr><td>Stopdatum</td><td>:</td><td></td></tr>'
+	                      + '<tr><td>Dosering</td><td>:</td><td>1 x per dag 100 milligram</td></tr>'
+	                      + '<tr><td>Toelichting</td><td>:</td><td></td></tr>'
+	                      + '<tr><td>Toediening</td><td>:</td><td>Oraal</td></tr>'
+	                      + '<tr><td>Voorschrijver</td><td>:</td><td>Lorsheyd, A<br />CAR 03053035</td></tr>');
+	else if (vIndex == 3)
+		ShowPrescription ('furosemide tablet 40mg',
+	                        '<tr><td>Startdatum</td><td>:</td><td>08-09-2016</td></tr>'
+	                      + '<tr><td>Stopdatum</td><td>:</td><td></td></tr>'
+	                      + '<tr><td>Dosering</td><td>:</td><td>1 x per dag 100 milligram</td></tr>'
+	                      + '<tr><td>Toelichting</td><td>:</td><td>Chronisch</td></tr>'
+	                      + '<tr><td>Toediening</td><td>:</td><td>Oraal</td></tr>'
+	                      + '<tr><td>Voorschrijver</td><td>:</td><td>Lorsheyd, A<br />CAR 03053035</td></tr>');
+}
---
+++ js/MediGen.js
@@ -0,0 +1,187 @@
+//--------------------------------------------------------------------
+// (c) 2017, MedLex
+//
+var g_bDeviceIsReady	= false;
+var __divName;
+
+//--------------------------------------------------------------
+// Wait for Cordova to load
+//
+function onLoad()
+{
+	g_bDeviceIsReady = false;
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+//---------------------------------------------------------------
+// Cordova is ready
+//
+function onDeviceReady()
+{
+	g_bDeviceIsReady = true;
+	BuildOverzicht ();
+}
+
+function isDeviceReady ()
+{
+	return g_bDeviceIsReady;
+}
+
+//------------------------------------------------------------------------------------------------------
+// Bedek alle onderliggende zaken met een semi-transparante waas
+// Deze krijgt standaard als id '__brCover'. Die wordt later weggegooid bij de OK knop.
+//
+function Cover ()
+{
+    
+    elemCover = document.createElement ('div');
+        
+    elemCover.style.cssText = 'position:absolute;left:0px;right:0px;top:0px;bottom:0px;opacity:0.2;background:#000;';
+    elemCover.id = '__brCover';
+    elemCover.style.transition = 'opacity 0.5s ease';
+    elemCover.style.webkitTransition = 'opacity 0.5s ease';
+    document.body.appendChild (elemCover);
+}
+
+//------------------------------------------------------------------------------------------------------
+// Geef een alert
+//
+function myAlert (szText)
+{
+
+    var elemWrapper;
+    var elemDiv;
+
+    Cover ();    						// onderliggende tekst even bedekken
+    elemWrapper = document.createElement ('div');		// wrapper voor alles
+    elemWrapper.id = '__myAlert';				// met deze ID. Kunnen we hem straks bij de OK knop terugvinden om weg te gooien
+    elemWrapper.style.cssText = 'position:absolute;width:80%;top:50%;left:50%;height:auto;background-color:#ffffff;padding:0;opacity:0;-moz-opacity:0;-khtml-opacity:0;overflow:hidden;';
+    elemWrapper.style.transition = 'opacity 0.5s ease';
+    elemWrapper.style.webkitTransition = 'opacity 0.5s ease';
+    elemDiv = document.createElement ('div');
+    elemDiv.style.cssText = 'position:relative;width:100%;height:auto;padding-top:10px;padding-bottom:10px;border-bottom:solid 2px #00a651;font-family:arial, helvetica, sans-serif;'
+                          + 'font-size:large;text-align:left;color:#00a651;background-color:#ffffff;padding-left:15px;';
+    elemDiv.innerHTML = 'Let op!';
+    elemWrapper.appendChild (elemDiv);
+    elemDiv = document.createElement ('div');
+    elemDiv.id = '__brAlertText';
+    elemDiv.style.cssText = 'position:relative;left:0px;right:0px;height:auto;padding-top:15px;padding-bottom:20px;border-bottom:solid 2px #afafaf;font-family:arial, helvetica, sans-serif;'
+                          + 'font-size:medium;text-align:left;color:#000000;background-color:#ffffff;padding-left:15px;padding-right:15px;';
+    elemDiv.innerHTML = szText;
+    elemWrapper.appendChild (elemDiv);
+    elemDiv = document.createElement ('div');
+    elemDiv.style.cssText = 'position:relative;width:100%;height:auto;padding-top:10px;padding-bottom:10px;border-bottom:solid 1px #afafaf;font-family:arial, helvetica, sans-serif;'
+                          + 'font-size:medium;text-align:center;color:#000000;background-color:#ffffff;';
+    elemDiv.setAttribute('onclick', 'onClickOK(\'__myAlert\');');
+    elemDiv.setAttribute('onmouseup', 'onClickOK(\'__myAlert\');');
+    elemDiv.innerHTML = 'OK';
+    elemDiv.onmouseover = function ()
+    {
+    	this.style.backgroundColor = '#afafaf';
+    };
+    elemDiv.onmouseout = function ()
+    {
+    	this.style.backgroundColor = '#ffffff';
+    };
+    elemWrapper.appendChild (elemDiv);
+    document.body.appendChild (elemWrapper);
+    
+    var vWidth  = elemWrapper.offsetWidth;
+    var vHeight = elemWrapper.offsetHeight;
+    vWidth = parseInt (vWidth/2, 10);
+    vHeight = parseInt (vHeight/2, 10);
+    elemWrapper.style.marginLeft = '-' + vWidth + 'px';
+    elemWrapper.style.marginTop = '-' + vHeight + 'px';
+    
+    elemWrapper.style.opacity = '1';
+    elemWrapper.style.mozOpacity = '1';
+    elemWrapper.style.khtmlOpacity = '1';
+}
+
+//------------------------------------------------------------------------------------------------------
+// Geef een alert
+//
+function ShowPrescription (szHeader, szText)
+{
+
+    var elemWrapper;
+    var elemDiv;
+	var szHTML;
+
+    Cover ();    						// onderliggende tekst even bedekken
+    elemWrapper = document.createElement ('div');		// wrapper voor alles
+    elemWrapper.id = '__myPrescription';				// met deze ID. Kunnen we hem straks bij de OK knop terugvinden om weg te gooien
+    elemWrapper.style.cssText = 'position:absolute;width:80%;top:50%;left:50%;height:auto;background-color:#ffffff;padding:0;opacity:0;-moz-opacity:0;-khtml-opacity:0;overflow:hidden;box-shadow: 12px 12px 8px grey;';
+    elemWrapper.style.transition = 'opacity 0.5s ease';
+    elemWrapper.style.webkitTransition = 'opacity 0.5s ease';
+    elemDiv = document.createElement ('div');
+    elemDiv.style.cssText = 'position:relative;width:100%;height:auto;padding-top:10px;padding-bottom:10px;border-bottom:solid 1px #afafaf;font-family:arial, helvetica, sans-serif;'
+                          + 'font-size:large;text-align:left;color:#000000;background-color:#FF9800;padding-left:15px;';
+    elemDiv.innerHTML = szHeader;
+    elemWrapper.appendChild (elemDiv);
+    elemDiv = document.createElement ('div');
+    elemDiv.id = '__brAlertText';
+    elemDiv.style.cssText = 'position:relative;left:0px;right:0px;height:auto;padding-top:15px;padding-bottom:20px;border-bottom:solid 1px #afafaf;font-family:arial, helvetica, sans-serif;'
+                          + 'font-size:medium;text-align:left;color:#000000;background-color:#ffffff;padding-left:15px;padding-right:15px;';
+    szHTML  = '<table>';
+    szHTML += szText;
+    szHTML += '</table>';
+    elemDiv.innerHTML = szHTML;
+    elemWrapper.appendChild (elemDiv);
+    elemDiv = document.createElement ('div');
+    elemDiv.style.cssText = 'position:relative;width:100%;height:auto;padding-top:10px;padding-bottom:10px;border-bottom:solid 1px #afafaf;font-family:arial, helvetica, sans-serif;'
+                          + 'font-size:medium;text-align:center;color:#000000;background-color:#ffffff;';
+    elemDiv.setAttribute('onclick', 'onClickOK(\'__myPrescription\');');
+    elemDiv.setAttribute('onmouseup', 'onClickOK(\'__myPrescription\');');
+    elemDiv.innerHTML = 'OK';
+    elemDiv.onmouseover = function ()
+    {
+    	this.style.backgroundColor = '#afafaf';
+    };
+    elemDiv.onmouseout = function ()
+    {
+    	this.style.backgroundColor = '#ffffff';
+    };
+    elemWrapper.appendChild (elemDiv);
+    document.body.appendChild (elemWrapper);
+    
+    var vWidth  = elemWrapper.offsetWidth;
+    var vHeight = elemWrapper.offsetHeight;
+    vWidth = parseInt (vWidth/2, 10);
+    vHeight = parseInt (vHeight/2, 10);
+    elemWrapper.style.marginLeft = '-' + vWidth + 'px';
+    elemWrapper.style.marginTop = '-' + vHeight + 'px';
+    
+    elemWrapper.style.opacity = '1';
+    elemWrapper.style.mozOpacity = '1';
+    elemWrapper.style.khtmlOpacity = '1';
+}
+
+function onClickOK (szName)
+{
+	
+    var elemCover = document.getElementById ('__brCover');
+    var elemWrapper = document.getElementById (szName);
+	
+	if (elemWrapper)
+	{
+		elemWrapper.style.opacity = '0';
+		elemWrapper.style.mozOpacity = '0';
+		if (elemCover)
+		{
+			elemCover.style.opacity = '0';
+			elemCover.style.mozOpacity = '0';
+		}
+		__divName = szName;
+		setTimeout(function()
+		{
+			var divCover = document.getElementById ('__brCover');
+			var divWrapper = document.getElementById (__divName);
+			
+			if (divWrapper)
+				divWrapper.parentNode.removeChild (divWrapper);
+			if (divCover)
+				divCover.parentNode.removeChild (divCover);
+		}, 500);
+	}
+}
