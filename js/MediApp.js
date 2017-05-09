784e754d03d54c7e4b57dd9f65e094c9fc06f755
WimEngberts <wim.engberts@centric.eu>
2017-05-09 14:42:00 +0200

    Update MediApp.js

--- js/MediApp.js
+++ js/MediApp.js
@@ -107,4 +107,6 @@ function onNfc(nfcEvent)
 function nfcTagDetected (reading)
 {
 	myAlert (reading.tag.id); // alert the id of the NFC reading
-} - See more at: http://www.dogu.io/blog/technology/adding-rfid-capabilities-to-your-android-phonegap-application/#sthash.JQ1T8QKW.dpuf
+}
+
+// See more at: http://www.dogu.io/blog/technology/adding-rfid-capabilities-to-your-android-phonegap-application/#sthash.JQ1T8QKW.dpuf
