07aba1b037cd39e9f47da2816ad63f2ecb2691c3
WimEngberts <wim.engberts@centric.eu>
2017-06-06 11:52:20 +0200

    Add files via upload

--- js/MediGen.js
+++ js/MediGen.js
@@ -38,7 +38,7 @@ function onDeviceReady()
 
 	window.plugins.intent.getCordovaIntent(function (Intent)
 	{
-		alert ('Action = ' + Intent.action + '\r\nData = \'' + Intent.data + '\'');
+		alert ('Action = ' + Intent.action + '\r\nData = \'' + Intent.data + '\'\r\nType = \'' + Intent.type + '\'');
     }, function ()
 	{
         alert ('Error getting the intent');
