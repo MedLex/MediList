dd845d34d954ef9a90d869fbf7cac78328dbbe87
WimEngberts <wim.engberts@centric.eu>
2018-01-17 17:00:46 +0100

    Add files via upload

--- js/MediApp.js
+++ js/MediApp.js
@@ -263,14 +263,8 @@ function plus ()
 	if (screenID == 0)						// medicatielijst
 	{
 		alert ('we beginnen.....');
-		if (!cordova)
-			alert ('maar cordova is er niet!!!');
-		else if (!cordova.plugins)
-			alert ('maar Cordova kent geen plugins');
-		else if (!cordova.plugins.barcodeScanner)
-			alert ('maar Cordova kent geen barcodeScanner plugin');
-		else
-			alert ('En we gaan nu scannen');
+		if (!scanner)
+			alert ('maar de scanner is er niet!!!');
 
 //		cordova.plugins.barcodeScanner.scan(
 		scanner.scan(
